dcb97ed3261d700ce75323bc3bd5f7d62e106a75
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-13 23:43:35 +0200

    Starting up the app, added logos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import './App.css';
+
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       
+
+      <Login />
+
+
+
+
     </div>
   );
 }
