8c388da367af423470569203ff511c622fa2de92
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-14 18:21:19 +0200

    Added styling to login page + user spotify auth

--- src/Login.js
+++ src/Login.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Styles/Login.scss';
 
 import Spotify_Green from './Images/Spotify_Green.png';
+import { loginUrl } from './spotify';
 
 function Login() {
     return (
         <div className="login">
             <img src={Spotify_Green} alt="Green" />
-            <button>Login with Spotify</button>
+            <button><a href={loginUrl}>Login with Spotify</a></button>
         </div>
     )
 }
