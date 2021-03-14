b2af66a3dc4256c151ae2e30cba493c39fe10ea8
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-14 18:59:50 +0200

    Added SpotifyWebApi + landing page after login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Styles/App.scss';
+import SpotifyWebApi from "spotify-web-api-js";
 
+import { getToken } from "./spotify";
 import Login from "./Login";
+import Main from "./Main";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getToken();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+    }
+  }, []);
+
   return (
+
     <div className="App">
-
-      <Login />
-
+      { token ? (<Main />) : (<Login />) }
     </div>
+
   );
 }
 
