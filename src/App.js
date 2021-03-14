65772aab1bf153c44c68773d011b512f17192a86
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-14 19:34:00 +0200

    Added Context API

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import SpotifyWebApi from "spotify-web-api-js";
 import { getToken } from "./spotify";
 import Login from "./Login";
 import Main from "./Main";
+import { DataLayer, useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
+  //Setting user token to state
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
 
+  //Catching usertoken from return URL, hiding it and
+  //placing it to state and using it
   useEffect(() => {
     const hash = getToken();
     window.location.hash = "";
@@ -29,6 +34,7 @@ function App() {
 
   return (
 
+    //If theres a token from login, show Main, otherwise show login
     <div className="App">
       { token ? (<Main />) : (<Login />) }
     </div>
