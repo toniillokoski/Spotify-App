f1aef0723e1a9ef3b2e3e5059994c3b2acee3d6d
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-15 22:57:58 +0200

    Sidebar = fixed, added songlist

--- src/SongRow.js
+++ src/SongRow.js
@@ -4,9 +4,13 @@ import './Styles/SongRow.scss';
 function SongRow({ track }) {
     return (
         <div className="songrow">
-            <img src="" alt=""/>
+            <img src={track.album.images[0].url} alt="album" className="albumimage" />
             <div className="songinfo">
                 <h1>{track.name}</h1>
+                <p>
+                    {track.artists.map((artist) => artist.name).join(", ")} -{" "}
+                    {track.album.name}
+                </p>
             </div>
         </div>
     )
